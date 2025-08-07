af4a92da8908eecb3bb51c8e80343ea01ba7d2be
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-08 04:58:59 +0530

    hero section done

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,15 @@ import AboutPage from "@/pages/about";
 
 function App() {
   return (
+    <>
     <Routes>
-      <Route element={<IndexPage />} path="/" />
+      <Route element={<IndexPage />} path="/"/>
       <Route element={<DocsPage />} path="/docs" />
       <Route element={<PricingPage />} path="/pricing" />
       <Route element={<BlogPage />} path="/blog" />
       <Route element={<AboutPage />} path="/about" />
     </Routes>
+    </>
   );
 }
 
