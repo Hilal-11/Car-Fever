74f5115e88598f1e03fe3f0a3b3cae3da2970e48
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-08 04:59:11 +0530

    hero section done

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,34 +15,45 @@ import clsx from "clsx";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
-import { Logo } from "@/components/icons";
 
 import { RiGithubFill } from "react-icons/ri";
 import { RiTwitterXFill } from "react-icons/ri";
+import {motion} from 'motion/react'
 
 export const Navbar = () => {
   return (
-    <HeroUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+    <HeroUINavbar  position="sticky" className="h-0">
+      <div className="relative w-full flex justify-center items-center">
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: -100
+        }}
+        animate={{
+          opacity: 1,
+          y: 1,
+        }}
+        transition={{
+          duration: 0.4,
+          delay: 0.2,
+
+        }}
+      className="absolute top-3 lg:top-6 w-full flex justify-between items-center bg-neutral-200 dark:bg-neutral-950 rounded-full py-3 lg:py-4 px-2 lg:px-10">
         <NavbarBrand className="gap-3 max-w-fit">
           <Link
             className="flex justify-start items-center gap-1"
             color="foreground"
             href="/"
           >
-            <Logo />
-            <p className="font-bold text-inherit">SASS</p>
+            {/* <Logo /> */}
+            <p className="lg:text-xl inter-bold">PrimeDeck</p>
           </Link>
         </NavbarBrand>
-        <div className="hidden lg:flex gap-4 justify-start ml-2">
+        <div className="px-10 hidden lg:flex gap-6 justify-start ml-2 inter-medium">
           {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
+            <NavbarItem className="" key={item.href}>
               <Link
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
-                )}
-                color="foreground"
+                className="text-neutral-500 text-[15px] roboto-regular"
                 href={item.href}
               >
                 {item.label}
@@ -50,13 +61,13 @@ export const Navbar = () => {
             </NavbarItem>
           ))}
         </div>
-      </NavbarContent>
+  
 
       <NavbarContent
         className="hidden sm:flex basis-1/5 sm:basis-full"
         justify="end"
       >
-        <NavbarItem className="hidden sm:flex gap-2">
+        <NavbarItem className="hidden sm:flex gap-4">
           <Link isExternal href={siteConfig.links.twitter} title="Twitter">
             <RiGithubFill size={22} className="text-default-500" />
           </Link>
@@ -67,13 +78,12 @@ export const Navbar = () => {
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github}>
-          <RiGithubFill size={22} className="text-default-500" />
-        </Link>
+      <NavbarContent className="sm:hidden basis-1 pl-4 pr-3" justify="end">
         <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
+      </motion.div>
+    </div>
 
       <NavbarMenu>
         <div className="mx-4 mt-2 flex flex-col gap-2">
