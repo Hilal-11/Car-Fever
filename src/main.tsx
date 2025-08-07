853c2d828b26a6823a4dfecc642155a51e666a65
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-08 04:59:39 +0530

    hero section done

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,13 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider>
-        <App />
+        <div style={{
+          backgroundImage: 'radial-gradient(circle at 0.5px 0.5px , rgba(90,110,114,0.7) 1px , transparent 0.5px)',
+          backgroundSize: "10px 10px",
+          backgroundRepeat : "repeat"
+        }}>
+          <App />
+        </div>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
