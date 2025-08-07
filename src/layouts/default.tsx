e515cd23deb4941818b537fb0946edb5aee8dfba
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-08 04:59:25 +0530

    hero section done

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@heroui/link";
 
 import { Navbar } from "@/components/navbar";
 
@@ -7,8 +8,17 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex flex-col h-screen">
+    <div className="relative flex flex-col  h-[2014px] "
+      style={{
+          backgroundImage: 'radial-gradient(circle at 0.5px 0.5px , rgba(90,110,114,0.7) 1px , transparent 0.5px)',
+          backgroundSize: "10px 10px",
+          backgroundRepeat : "repeat"
+      }}
+    >
       <Navbar />
+      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16 ">
+        {children}
+      </main>
     </div>
   );
 }
