77d5e4db1f03f6e47a4752af25d79e1c6eef982f
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-16 00:13:16 +0530

    Some UI changes

--- src/components/BookcallAndSignupBox.tsx
+++ src/components/BookcallAndSignupBox.tsx
@@ -8,8 +8,8 @@ function BookcallAndSignupBox() {
                 <h1 className='text-4xl lg:tcursor-pointerext-5xl Inter-bold '>Start now, <br /> no strings attached</h1>
                 <p className='text-md py-2 Inter-medium text-[16px]'>Integrate complete user management in minutes. Free for your first 10,000 monthly active users and 100 monthly active orgs. No credit card required.</p>
                 <div className='flex lg:gap-5 gap-2 py-6'>
-                    <buttons className="Inter-bold cursor-pointer rounded-2xl px-5 lg:px-14 py-[6px] bg-white dark:bg-neutral-900 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">Book Call</buttons>
-                    <buttons className="Inter-bold cursor-pointer rounded-2xl px-5 lg:px-14 py-[6px] bg-white dark:bg-neutral-900 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">Signup</buttons>
+                    <button className="Inter-bold cursor-pointer rounded-2xl px-5 lg:px-14 py-[6px] bg-white dark:bg-neutral-900 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">Book Call</button>
+                    <button className="Inter-bold cursor-pointer rounded-2xl px-5 lg:px-14 py-[6px] bg-white dark:bg-neutral-900 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">Signup</button>
                 </div>
             </div>
             <div className='flex items-center lg:justify-center justify-center lg:pr-10'>
