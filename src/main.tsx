876751a11d5c48b333d05b7710629230b8885f29
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-16 15:57:43 +0530

    authntication

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,19 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
 import "@/styles/globals.css";
+import { ClerkProvider } from '@clerk/clerk-react'
 
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <Provider>
           <div style={{
             backgroundImage: 'radial-gradient(circle at 0.5px 0.5px , rgba(90,110,114,0.7) 1px , transparent 0.5px)',
@@ -19,6 +27,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
               <App />
           </div>
       </Provider>
+      </ClerkProvider> 
     </BrowserRouter>
   </React.StrictMode>,
 );
