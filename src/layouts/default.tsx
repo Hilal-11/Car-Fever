0054a48f279e25052f5f343e58a76b916e4571b3
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-17 17:58:35 +0530

    404 page and header bug

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -8,13 +8,7 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex flex-col"
-      style={{
-          // backgroundImage: 'radial-gradient(circle at 0.5px 0.5px , rgba(90,110,114,0.7) 1px , transparent 0.5px)',
-          // backgroundSize: "10px 10px",
-          // backgroundRepeat : "repeat"
-      }}
-    >
+    <div className="relative flex flex-col">
       <Navbar />
       <main className="container mx-auto max-w-7xl px-0 flex-grow pt-16 ">
         {children}
