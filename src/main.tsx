7a7234d195ac56df18e0abb3f8b78177ca7c4399
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-17 17:58:45 +0530

    404 page and header bug

--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <Provider>
-          <div style={{
+          <div className="z-40" style={{
             backgroundImage: 'radial-gradient(circle at 0.5px 0.5px , rgba(90,110,114,0.7) 1px , transparent 0.5px)',
             backgroundSize: "10px 10px",
             backgroundRepeat : "repeat"
